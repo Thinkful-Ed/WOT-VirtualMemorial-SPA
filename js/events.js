4f3c4f1cbda45f1a49905e85fabc8af6162df8f5
andrewPavlick <aepavlick@gmail.com>
2017-06-16 17:16:14 -0400

    Major rework of the UI along with polish work. Search result still needs a bit of work but overall solid. Viewport transitions still needs a bit of work.

--- js/events.js
+++ js/events.js
@@ -3,12 +3,21 @@ $(document).ready(function(){
     dReset();
     dWebgl();
 });
-$('#js-tour').click(function(){
-    dWebgl();
-});
-$('#js-search').click(function(){
-    dSearch();
-});
-$('#js-stories').click(function(){
-    dInfo();
+    $('#js-tour').click(function(){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+    });
+    $('#js-search').click(function(){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+    });
+    $('#js-stories').click(function(){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
 });
--- js/templates.js
+++ js/templates.js
@@ -2,40 +2,7 @@
  * Created by Andrew on 6/11/2017.
  */
 var templates = {
-    webgl: [
-        '<section id="webgl" class="win bg-white"></section>'
-    ],
-    search: [
-    '<section id="search" class="win bg-white">'+
-        '<div class="bg-red search-box">'+
-            '<input type="text">'+
-            '<button>Search</button>'+
-        '</div>'+
-            '<div class="info">Edmunds John Joseph</div>'+
-            '<div class="info">Stonesifer Kristofor T.</div>'+
-            '<div class="info">Maria Giovanny</div>'+
-            '<div class="info">Prosser Brain C.</div>'+
-            '<div class="info">David Jefferson D.</div>'+
-            '<div class="info">Petithory Danieal H.</div>'+
-            '<div class="info">Chapman Nathan R.</div>'+
-            '<div class="info">Davis Bryan L.</div>'+
-            '<div class="info">Johnson Benjamin</div>'+
-            '<div class="info">Jakes Jr. Michael J.</div>'+
-        '<div>'+
-        '</div>'+
-    '</section>'
-    ],
-    story: [
-    '<section id="story" class="win bg-white">' +
-        '<div class="info">Name: <p id="name">Marshall John Winston</p></div>'+
-        '<div class="info">Age: <p id="age">50</p></div>'+
-        '<div class="info">From: <p id="from">Los Angeles California</p></div>'+
-        '<div class="info">Country: <p id="country">United States</p></div>'+
-        '<div class="info">Rank: <p id="rank">Sergant 1st Class</p></div>'+
-        '<div class="info">Branch: <p id="branch">U.S. Army</p></div>'+
-        '<div class="info">Unit: <p id="unit">3rd BN 15th INF REG 3rd Infantry Division</p></div>'+
-        '<div class="info">Station: <p id="station">Fort Stewart, GA</p></div>'+
-        '<div class="info">Date of Death: <p id="dod">2003-04-08</p></div>'+
-    '</section>'
-    ]
+    webgl: function(){},
+    search: function(){},
+    story: function(){}
 };
