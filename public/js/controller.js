ecdefd13ad757922e3492e9b58706c377836ae3c
andrewPavlick <aepavlick@gmail.com>
2017-08-03 16:19:48 -0400

    Adding files for the controller and html templates.

---
+++ public/js/controller.js
@@ -0,0 +1,3 @@
+var crtl = (function(){
+    // Coming Soon!
+}());
---
+++ public/js/htmlTemplates.js
@@ -0,0 +1,8 @@
+var htmlTemplates = (function(){
+    let searchResults = function(){};
+    let searchInfo = function(){};
+     return{
+         searchResults: searchResults,
+         searchInfo: searchInfo
+     };
+}());
