1f3444ba985d4335c563658df9d5492312ae23ed
andrewPavlick <aepavlick@gmail.com>
2017-10-06 09:49:02 -0400

    Commented out compression. Blocking DB query. Will address later.

--- server.js
+++ server.js
@@ -18,21 +18,12 @@ app.use(express.static(__dirname+'/public'));
 
 // Middleware
 app.use(morgan('common'));
-// CORS
-/*app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    if (req.method === 'OPTIONS') {
-        return res.send(204);
-    }
-    next();
-});*/
 app.use(bodyParser.json());
-app.use(compression);
-app.use(passport.initialize()); // Init passport
-passport.use(basicStrategy); // Register basicStrategy
+// app.use(passport.initialize());
+// passport.use(basicStrategy);
 // passport.use(jwtStrategy);
+//app.use(compression);
+
 
 // Endpoints or Routing
 app.use('/veterans', routerStoryText);
